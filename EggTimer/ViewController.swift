4d65421a2589ccf7deff9e6010ca558dc2950a72
torinunna <torinunna9588@gmail.com>
2022-03-04 23:38:41 +0900

    challenge - use dictionary to print out the correct time for the hardness that’s selected.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+//    let softTime = 5
+//    let mediumTime = 7
+//    let hardTime = 12
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
         
 //        if hardness == "Soft"{
 //            print(softTime)
@@ -26,17 +28,20 @@ class ViewController: UIViewController {
 //            print(hardTime)
 //        }
         
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("Error")
-        }
+//        switch hardness {
+//        case "Soft":
+//            print(softTime)
+//        case "Medium":
+//            print(mediumTime)
+//        case "Hard":
+//            print(hardTime)
+//        default:
+//            print("Error")
+//        }
+        
+        print(eggTimes[hardness])
     }
-
-
+    
 }
+
+
